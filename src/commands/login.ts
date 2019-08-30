5dfeb34ecfd8c35e4bc7b88a7696616d0ba7d00a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-08-30 12:10:52 -0400

    Persist the obtained token to netrc

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,5 +1,6 @@
 import { Command, flags } from "@oclif/command"
 import cli from "cli-ux"
+import netrc from "netrc-parser"
 import Gravity from "../lib/gravity"
 
 export default class Login extends Command {
@@ -19,12 +20,20 @@ export default class Login extends Command {
     const email = await cli.prompt("Email", { type: "normal" })
     const password = await cli.prompt("Password", { type: "hide" })
 
-    this.log(`Authenticating against stagingapi.artsy.net for ${email}...`)
+    this.log(`Authenticating against ${Gravity.HOSTS.staging} for ${email}...`)
 
     const result = await new Gravity().getAccessToken({
       email,
       password,
     })
+
+    // TODO: allow switching env
+    netrc.loadSync()
+    netrc.machines[Gravity.HOSTS.staging] = {
+      login: email,
+      password: result.access_token,
+    }
+    netrc.saveSync()
 
     this.log("-------------- vvv Your access token vvv --------------")
     this.log(result.access_token)
